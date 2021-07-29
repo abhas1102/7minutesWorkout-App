fffe0f203a448734c203a258aa3b8a44bf654d35
Abhas Kumar <abhas1102@gmail.com>
2021-07-30 01:58:47 +0530

    Created custom button shape and added ripple effect on clicking for the first UI screen.

--- app/src/main/java/com/example/dailyworkoutfor7minutes/MainActivity.kt
+++ app/src/main/java/com/example/dailyworkoutfor7minutes/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.dailyworkoutfor7minutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        llStart.setOnClickListener {
+            Toast.makeText(this,"Let's ready to be bored!!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
