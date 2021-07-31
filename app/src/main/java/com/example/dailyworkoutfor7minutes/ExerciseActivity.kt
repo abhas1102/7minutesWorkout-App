4b7009cef21c5cb258c127b7010fe112eabad8c5
Abhas Kumar <abhas1102@gmail.com>
2021-07-31 12:55:06 +0530

    Implemented countdown timer for exercises and make it visible on the same screen after the rest timer is finished.

--- app/src/main/java/com/example/dailyworkoutfor7minutes/ExerciseActivity.kt
+++ app/src/main/java/com/example/dailyworkoutfor7minutes/ExerciseActivity.kt
@@ -1,8 +1,10 @@
 package com.example.dailyworkoutfor7minutes
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import kotlinx.android.synthetic.main.activity_exercise.*
@@ -10,7 +12,9 @@ import kotlinx.android.synthetic.main.activity_exercise.*
 class ExerciseActivity : AppCompatActivity() {
 
     private var restTimer:CountDownTimer? = null
+    private var exerciseTimer:CountDownTimer? = null
     private var restProgress = 0
+    private var workProgress = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
@@ -26,6 +30,8 @@ class ExerciseActivity : AppCompatActivity() {
         }
 
         setupRestView()
+
+
     }
 
     override fun onDestroy() {
@@ -33,8 +39,15 @@ class ExerciseActivity : AppCompatActivity() {
             restTimer!!.cancel()
             restProgress = 0
         }
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            workProgress = 0
+        }
         super.onDestroy()
     }
+
+
+
 
     private fun setRestProgressBar(){
         progressBar.progress = restProgress
@@ -46,7 +59,25 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity,"Start working", Toast.LENGTH_SHORT).show()
+             //   Toast.makeText(this@ExerciseActivity,"Start working", Toast.LENGTH_SHORT).show()
+                setupExerciseView()
+            }
+
+        }.start()
+    }
+
+    private fun setWorkProgressBar(){
+
+        exercise_progressBar.progress = workProgress
+        exerciseTimer = object :CountDownTimer(30000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                workProgress++
+                exercise_progressBar.progress = 30-workProgress
+                exercise_tvTimer.text = (30-workProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Good work", Toast.LENGTH_SHORT).show()
             }
 
         }.start()
@@ -58,5 +89,17 @@ class ExerciseActivity : AppCompatActivity() {
             restProgress = 0
         }
         setRestProgressBar()
+    }
+
+
+
+    private fun setupExerciseView(){
+        llRestView.visibility = View.GONE
+        llExerciseView.visibility = View.VISIBLE
+        if(exerciseTimer!=null){
+           exerciseTimer!!.cancel()
+            workProgress = 0
+        }
+        setWorkProgressBar()
     }
 }
